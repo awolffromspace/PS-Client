038343345c693676e18d604f61e2e1f43f23ee29
awolffromspace <ewolft@gmail.com>
2021-03-28 22:03:10 -0400

    Use PC settings instead of localhost

--- config/config-example.js
+++ config/config-example.js
@@ -18,8 +18,8 @@ Config.whitelist = [
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
-	id: 'localhost',
-	host: 'localhost',
+	id: 'pokecommunity',
+	host: 'battle.pokecommunity.com',
 	port: 8000,
 	httpport: 8000,
 	altport: 80,
